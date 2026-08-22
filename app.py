f73395291580e7a96a6f4446415175a81839cee9
williamfda-web <williamfda@gmail.com>
2026-08-22 09:12:08 -0300

    Modify page title and add footer caption
    Updated page title and added a footer caption.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Configuração da página
 st.set_page_config(
-    page_title="Calculadoras de Saúde",
+    page_title="Calculadoras de Saúde by William",
     page_icon="💪",
     layout="centered"
 )
